6a76fad149722b40f0adef331eba0a48d5914ede
itguytrades <itguytrades@proton.me>
2024-08-19 16:31:18 -0700

    2. Smoke Test

---
+++ scripts/1_deploy.js
@@ -0,0 +1,19 @@
+const hre = require("hardhat");
+
+async function main() {
+  // fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
+
+  // deploy contract
+
+  const token = await Token.deploy()
+  await token.deployed()
+  console.log(`Token Deployed to: ${token.address}`)
+}
+
+main()
+ .then(() => process.exit(0))
+ .catch((error) => {
+  console.error(error);
+  process.error(1);
+ });
