4759a215c8907016b76ade90b6ae6faa0b1133af
itguytrades <itguytrades@proton.me>
2024-08-21 09:16:27 -0700

    4. Token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -27,6 +27,13 @@ contract Token {
 		balanceOf[msg.sender] = totalSupply;
 	}
 
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		// Deduct tokens from sender
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		// Credit tokens to receiver
+		balanceOf[_to] = balanceOf[1] + _value;
+
+	}
 }
 
 
