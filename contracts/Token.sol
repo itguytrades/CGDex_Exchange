8427306fa7c8412e9a63ced6176db031746f285f
itguytrades <itguytrades@proton.me>
2024-08-21 09:06:55 -0700

    4. Token balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,12 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
+	// Track Balances
+
+	mapping(address => uint256) public balanceOf;
+
+	// Send Tokes
+
 	constructor(
 		string memory _name, 
 		string memory _symbol, 
@@ -18,6 +24,7 @@ contract Token {
 		name = _name;
 		symbol = _symbol;
 		totalSupply = _totalSupply * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
 	}
 
 }
--- test/Token.js
+++ test/Token.js
@@ -5,13 +5,16 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-let token
+let token, accounts, deployer
 
 describe('Token', () => {
 
   beforeEach(async () => {
   	const Token = await ethers.getContractFactory('Token')
   	token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+    
+    accounts = await ethers.getSigners()
+    deployer = accounts[0]
   })
 
   describe('Deployment', () => {
@@ -32,6 +35,10 @@ describe('Token', () => {
   })
    it('has correct total supply', async () => {
    	expect(await token.totalSupply()).to.equal(totalSupply)
+
+  })
+   it('assigns total supply to deployer', async () => {
+    expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 
   })
 
